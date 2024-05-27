0ebdce78d8276fadff8486040dc734e1a76f6172
Justin Garcia <justin.garcia@gmail.com>
2024-05-27 11:21:55 +0900

    update for the real site

--- foreground.js
+++ foreground.js
@@ -2,7 +2,7 @@ console.log("foreground.js loaded");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.command = "notetube:generate") {
-		window.location.href = `http://127.0.0.1:3000/generate?v=${window.location.href}`;
+		window.location.href = `https://www.notetube.xyz/generate?v=${window.location.href}`;
 		sendResponse(true)
 	}
 });
